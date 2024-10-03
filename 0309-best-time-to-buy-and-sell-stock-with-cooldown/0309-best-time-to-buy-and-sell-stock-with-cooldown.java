fa89bba19592446d96c8545fc77e853979a4cf2c
Aram Wondergem <108117968+AramWondergem@users.noreply.github.com>
2024-10-03 10:19:08 -0400

    Time: 1 ms (85.29%), Space: 41.8 MB (46.85%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,45 +1,28 @@
 class Solution {
-    int[][] memo;
-    int lastDay;
-    int[] prices;
-    
     public int maxProfit(int[] prices) {
-        this.lastDay = prices.length;
-        
-        if(lastDay <= 1){
+        int m = prices.length;
+
+        if(m <= 1) {
             return 0;
         }
-        
-        this.memo = new int[lastDay][2];
-        this.prices = prices;
-        
-        for(int i = 0; i < lastDay; i++){
-            Arrays.fill(memo[i], -1);
+
+        int[][] dp = new int[m + 2][2];
+
+        for(int day = m - 1; day >= 0; day--){
+            for(int holding = 0; holding < 2; holding++){
+
+                int ans = dp[day + 1][holding];
+
+                if(holding == 1) {
+                    ans = Math.max(ans, prices[day] + dp[day + 2][holding - 1]);
+                } else {
+                    ans = Math.max(ans, -prices[day] + dp[day + 1][holding + 1]);
+                }
+
+                 dp[day][holding] = ans;
+            }
         }
-        
-        return dp(0,0);
-        
-    }
-    
-    private int dp(int day, int holding){
-        if(day >= lastDay) {
-            return 0;
-        }
-        
-        if(memo[day][holding] != -1){
-            return memo[day][holding];
-        }
-        
-        int ans = dp(day + 1, holding); 
-        
-        if(holding == 1) {
-            ans = Math.max(ans, this.prices[day] + dp(day + 2, holding - 1));
-        } else {
-            ans = Math.max(ans, -this.prices[day] + dp(day + 1, holding + 1));
-        }
-        
-        this.memo[day][holding] = ans;
-        
-        return ans;   
+
+        return dp[0][0];
     }
 }
