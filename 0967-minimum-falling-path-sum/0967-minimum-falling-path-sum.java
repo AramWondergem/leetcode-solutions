310a5b83e05c490b0567d018ca5f92c7eb382255
Aram Wondergem <108117968+AramWondergem@users.noreply.github.com>
2024-10-08 14:14:28 -0400

    Time: 4 ms (73.73%), Space: 45.2 MB (52.67%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,52 +1,36 @@
 class Solution {
-    int n;
-    int[][] matrix;
-    int[][] memo;
-    int[] directions;
-    
     public int minFallingPathSum(int[][] matrix) {
-        this.n = matrix.length;
-        this.matrix = matrix;
-        this.memo = new int[n][n];
-        this.directions = new int[]{-1,0,1};
-        
-        for(int[] array: this.memo){
-            Arrays.fill(array, Integer.MAX_VALUE);
+        int n = matrix.length;
+        int[] dp = matrix[0];
+        int[] directions = new int[]{-1,0,1};
+
+        for(int row = 1; row < n; row++) {
+            int[] nextRow = new int[n];
+
+            for(int col = 0; col < n; col++) {
+                int ans = Integer.MAX_VALUE;
+                
+                for(int dir: directions){
+                    if(valid(col + dir, n)){
+                    ans = Math.min(ans, dp[col + dir]);
+                    }
+                }
+
+                nextRow[col] = ans + matrix[row][col];
+            }
+
+            dp = nextRow;
         }
         
         int ans = Integer.MAX_VALUE;
-        
-        for(int col = 0; col < n; col++){
-            ans = Math.min(ans, dp(n - 1, col));
+        for(int i : dp ) {
+            ans = Math.min(ans, i);
         }
         
         return ans;
     }
     
-    private int dp(int row, int col) {
-        if(row == 0) {
-            return this.matrix[row][col];
-        }
-        
-        if(this.memo[row][col] != Integer.MAX_VALUE){
-            return this.memo[row][col];
-            
-        }
-        
-        int ans = Integer.MAX_VALUE;
-        
-        for(int dir: this.directions){
-            if(valid(col + dir)){
-                ans = Math.min(ans, dp(row - 1, col + dir));
-            }
-        }
-        
-        this.memo[row][col] = ans + matrix[row][col];
-        
-        return this.memo[row][col];
-    }
-    
-    private boolean valid(int col){
-        return 0 <= col && col < this.n;
+    private boolean valid(int col, int n){
+        return 0 <= col && col < n;
     }
 }
